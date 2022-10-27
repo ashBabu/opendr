e82a2e803663fb5a661e94df16a9aad9e386354c
aselimc <canakcia@cs.uni-freiburg.de>
2022-10-27 18:09:38 +0200

    Add EfficientLPS module for tests

--- src/opendr/perception/panoptic_segmentation/__init__.py
+++
@@ -1,4 +0,0 @@
-from opendr.perception.panoptic_segmentation.datasets import CityscapesDataset, KittiDataset
-from opendr.perception.panoptic_segmentation.efficient_ps import EfficientPsLearner
-
-__all__ = ['CityscapesDataset', 'KittiDataset', 'EfficientPsLearner']
