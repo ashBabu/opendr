4c134652efb505ac940561b7c7151bc5f372d70c
sycz00 <fabian.schmalstieg@gmx.de>
2022-12-23 00:27:16 +0100

    typo

--- src/opendr/control/multi_object_search/__init__.py
+++ src/opendr/control/multi_object_search/__init__.py
@@ -1,6 +1,6 @@
-from opendr.control.multi_object_search.multi_object_search_learner import MultiObjectSearchRLLeaner
+from opendr.control.multi_object_search.multi_object_search_learner import MultiObjectSearchRLLearner
 from opendr.control.multi_object_search.algorithm.evaluation import evaluation_rollout
 from opendr.control.multi_object_search.algorithm.igibson.multi_object_env import MultiObjectEnv
 
 
-__all__ = ['MultiObjectSearchRLLeaner', 'MultiObjectEnv', 'evaluation_rollout']
+__all__ = ['MultiObjectSearchRLLearner', 'MultiObjectEnv', 'evaluation_rollout']
